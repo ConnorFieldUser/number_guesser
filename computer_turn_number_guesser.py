48934c3a9f2b2a524bfbb85bf59b3d66a34215f1
ConnorFieldUser <conquest.field@gmail.com>
2017-01-04 13:48:41 -0500

    lowered range to 50 and prints the computers guess

--- computer_turn_number_guesser.py
+++ computer_turn_number_guesser.py
@@ -3,14 +3,15 @@ import random
 
 
 def computer_number_guesser():
-    player_number = int(input("Pick a number between 1 and 100 for the computer to guess: "))
+    player_number = int(input("Pick a number between 1 and 50 for the computer to guess: "))
     tries_left = 5
     low = 0
-    high = 100
-    if player_number > 0 and player_number < 100:
+    high = 50
+    if player_number > 0 and player_number < 50:
         for guess in range(5):
             computer_guess = random.randint(low, high)
             tries_left -= 1
+            print(computer_guess)
             if computer_guess == player_number:
                 print("Computer wins! Your number was {}".format(player_number))
                 break
@@ -25,6 +26,6 @@ def computer_number_guesser():
         else:
             print("Game Over. You win!")
     else:
-        print("Between 1 and 100! Game over cheater.")
+        print("Between 1 and 50! Game over cheater.")
 
 computer_number_guesser()
